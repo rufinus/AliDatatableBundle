77265ba22d7a0182f574dd48f4caec896be8bead
Ludwig Ruderstaller <lr@cwd.at>
2014-06-03 18:33:20 +0200

    Integration fix

--- Util/Factory/Query/DoctrineBuilder.php
+++ Util/Factory/Query/DoctrineBuilder.php
@@ -95,7 +95,7 @@ class DoctrineBuilder implements QueryInterface
                 }
             }
 
-            if ($search) {
+            if ($search && count($like) > 0) {
                 $queryBuilder->andWhere(call_user_func_array(array($queryBuilder->expr(), 'orX'), $like));
                 $queryBuilder->setParameter('search', '%'.$query.'%');
             }
